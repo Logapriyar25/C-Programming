eb5e25ff677ea5aeadb20173137510b6522e699a
Logapriya R <102983138+Logapriyar25@users.noreply.github.com>
2022-07-01 19:42:29 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,272 @@
+/* Switch Statement
+switch(choice)
+{
+case value:
+statement 1
+break;
+case value:
+statement 2
+break;
+default:
+default statement
+break;
+}
+*/
+
+int main()
+{
+    int ch;
+    printf("\nEnter the value :");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+    case 1:
+        printf("\n One");
+        break;
+    case 2:
+        printf("\n Two");
+        break;
+    case 3:
+        printf("\n Three");
+        break;
+    default:
+        printf("\n Invalid No");
+        break;
+
+    }
+        return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
