61d3661ba73ff0e0a2dca33699ed15023eb24321
Logapriya R <102983138+Logapriyar25@users.noreply.github.com>
2022-07-29 15:43:51 +0530

    Update call_by_ref.c

--- call_by_ref.c
+++ call_by_ref.c
@@ -1,6 +1,11 @@
 
 //Call by reference function in C programming
 
+/*While calling a function, we pass values of variables to it. Such functions are known as “Call By Values”. 
+While calling a function, instead of passing the values of variables, we pass address of variables(location of variables) 
+to the function known as “Call By References.
+*/
+
 #include<stdio.h>
 
 void swap(int *x,int *y)
